d0f9519248fcd64c80edc1efaf3c3caf060d8fda
Keith <keithm@aoeex.com>
2016-07-05 18:09:34 -0400

    Make public directory and move public files there.

--- public/index.php
+++ public/index.php
@@ -6,12 +6,12 @@
  * @package Phile
  */
 
-require_once __DIR__ . '/lib/Phile/Bootstrap.php';
+require_once 'vendor/autoload.php';
 
 ob_start();
 
 try {
-    \Phile\Bootstrap::getInstance()->initializeBasics();
+    \Phile\Bootstrap::getInstance()->initializeBasics(\Phile\Core\Utility::load('config.php'));
     $router = new \Phile\Core\Router();
     $response = new \Phile\Core\Response();
     $phileCore = new \Phile\Core($router, $response);
